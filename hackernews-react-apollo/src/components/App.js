7649d6be3eccd5c8225c3a98d29593698f901571
YoLoADR <atticot.yohann@gmail.com>
2019-11-21 17:31:02 +0100

    Requêtes: Chargement des liens

---
+++ hackernews-react-apollo/src/components/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "../logo.svg";
+import "../styles/App.css";
+import LinkList from "./LinkList";
+
+function App() {
+  return <LinkList />;
+}
+
+export default App;
