0f2ccd9cda1e4256c0a91f9b33c9f651d2038a61
YoLoADR <atticot.yohann@gmail.com>
2019-11-22 08:57:24 +0100

    Mutations: Créer des liens

--- hackernews-react-apollo/src/components/App.js
+++ hackernews-react-apollo/src/components/App.js
@@ -2,9 +2,10 @@ import React from "react";
 import logo from "../logo.svg";
 import "../styles/App.css";
 import LinkList from "./LinkList";
+import CreateLink from "./CreateLink";
 
 function App() {
-  return <LinkList />;
+  return <CreateLink />;
 }
 
 export default App;
